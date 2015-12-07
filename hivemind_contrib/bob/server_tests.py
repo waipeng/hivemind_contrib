bb4728befb0387831222665bdc0c3c06328e6e65
Jake Yip <jake.yip@unimelb.edu.au>
2015-12-07 12:12:23 +1100

    Add comments for tests

--- hivemind_contrib/bob/server_tests.py
+++ hivemind_contrib/bob/server_tests.py
@@ -8,15 +8,20 @@ class ServerTestCase(unittest.TestCase):
         self.server = server
         super(ServerTestCase, self).__init__(testname)
 
+    # test if server is active
     def test_server_status_active(self):
         self.assertEqual(self.server.status, "ACTIVE", 
                 msg="Server status is not ACTIVE")
 
+    # test for active port state
     def test_port_state_active(self):
         interfaces = self.server.interface_list()
         self.assertEqual(interfaces[0].port_state, "ACTIVE", 
                 msg="Port state  is not ACTIVE")
 
+    # test for instance missing images
+    # useful for cases when instances are still running but the image is gone
+    # from /var/lib/nova/instances
     def test_image_not_found(self):
         if not hasattr(self.server, 'fault'):
             return True
@@ -24,6 +29,7 @@ class ServerTestCase(unittest.TestCase):
         m = re.match(r"^Image (.)+ could not be found.$", msg)
         self.assertIsNone(m, msg="Image not found")
 
+    # test if console logs returns anything
     def test_server_console_log(self):
         self.assertNotEqual(len(self.server.get_console_output()), 0,
                 msg="Console log has no output")
