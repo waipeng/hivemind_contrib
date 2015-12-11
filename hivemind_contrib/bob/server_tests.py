1c3ea0374393fa2052688437712132353fe1b69a
Jake Yip <jake.yip@unimelb.edu.au>
2015-12-11 15:02:30 +1100

    Add wip novnc test

--- hivemind_contrib/bob/server_tests.py
+++ hivemind_contrib/bob/server_tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import unittest, os, re
+import requests
 
 # This is either very clever or very stupid
 class ServerTestCase(unittest.TestCase):
@@ -17,7 +18,7 @@ class ServerTestCase(unittest.TestCase):
     def test_port_state_active(self):
         interfaces = self.server.interface_list()
         self.assertEqual(interfaces[0].port_state, "ACTIVE", 
-                msg="Port state  is not ACTIVE")
+                msg="Port state is not ACTIVE")
 
     # test for instance missing images
     # useful for cases when instances are still running but the image is gone
@@ -35,11 +36,27 @@ class ServerTestCase(unittest.TestCase):
         self.assertNotEqual(len(self.server.get_console_output()), 0,
                 msg="Console log has no output")
 
+    # test get vnc console
+    def test_server_get_novnc_url(self):
+        console = self.server.get_vnc_console('novnc')
+        self.assertTrue(console, msg="Unable to get novnc URL")
+
+    # test novnc
+    # TODO(jake): figure out how to test if console is working
+    def test_server_novnc(self):
+        console = self.server.get_vnc_console('novnc')
+        r = requests.get(console['console']['url'])
+        self.assertEqual(r.status_code, 200,
+                msg="novnc url response status code not 200")
+
+
 def run(server):
     suite = unittest.TestSuite()
     suite.addTest(ServerTestCase('test_server_status_active', server))
     suite.addTest(ServerTestCase('test_port_state_active', server))
     suite.addTest(ServerTestCase('test_server_console_log', server))
+    suite.addTest(ServerTestCase('test_server_get_novnc_url', server))
+    suite.addTest(ServerTestCase('test_server_novnc', server))
 
     # make unittest run quietly
     stream = open(os.devnull, 'w')
